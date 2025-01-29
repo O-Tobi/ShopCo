e3ce410b27b0b1245e547385dda61eac69e2a41f
unknown <dunjoyebiz2020@gmail.com>
2025-01-29 09:01:51 +0100

    setup for daisy done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import daisyui from 'daisyui';
 
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], 
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ["Integral Bold", "sans-serif"],
+        body: ["Satoshi", "sans-serif"],
+      },
+      colors: {
+        primary: 'oklch(0% 0 0)',
+        page: 'oklch(68.62% 0 0)',
+        whiteHeroBg: 'oklch(95.7% 0.0025 345.21)',
+        whiteFont: 'oklch(100% 0 0)',
+        productBg: 'oklch(95.04% 0.0025 48.72)',
+        secondaryHero: 'oklch(95.51% 0 0)',
+      }, 
+    },
+  },
+  plugins: [daisyui],
+
+  daisyui: {
+    themes: ["light"], // DaisyUI light theme
+  },
+};
