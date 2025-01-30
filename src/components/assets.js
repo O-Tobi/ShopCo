f85e38a1a5e2e5f128543babc5d30d7f344efa55
unknown <dunjoyebiz2020@gmail.com>
2025-01-30 22:57:56 +0100

    reached a checkpoint

--- src/components/assets.js
+++ src/components/assets.js
@@ -14,6 +14,17 @@ import a from "./../assets/arrow.svg"
 import l from "./../assets/line.svg";
 import bg from "./../assets/bigHero.png";
 import p1 from "./../assets/product1.png";
+import p1 from "./../assets/product1.png";
+import p1 from "./../assets/product1.png";
+import p2 from "./../assets/product2.png";
+import p3 from "./../assets/product3.png";
+import p4 from "./../assets/product4.png";
+import p5 from "./../assets/product5.png";
+import p6 from "./../assets/product6.png";
+import p7 from "./../assets/product7.png";
+import p8 from "./../assets/product8.png";
+import l2 from "./../assets/horizontalLine.svg";
+
 
 
 
@@ -33,3 +44,12 @@ export const Arrow = a;
 export const Line = l;
 export const BigHero = bg;
 export const Product1 = p1;
+export const Product2 = p2;
+export const Product3 = p3;
+export const Product4 = p4;
+export const Product5 = p5;
+export const Product6 = p6;
+export const Product7 = p7;
+export const Product8 = p8;
+export const HorizontalLine = l2;
+
