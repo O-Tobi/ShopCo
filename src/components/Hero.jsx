155db26b5d1f58f063bbe161f42c134d53bff14d
unknown <dunjoyebiz2020@gmail.com>
2025-02-05 11:41:10 +0100

    reached a checkpoint

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,34 +13,36 @@ import {
 const Hero = () => {
   return (
     <>
-      <div className="flex flex-wrap gap-[4rem] h-[663px] bg-whiteHeroBg w-screen">
+      <div className="flex flex-wrap lg:justify-center items-center gap-[4rem] lg:h-[663px] bg-whiteHeroBg w-screen">
         {/* the right section starts here */}
-        <div className="bp my-12">
-          <h1 className="font-heading text-[64px] leading-none">
-            FIND CLOTHES <br /> THAT MATCHES <br /> YOUR STYLE
-          </h1>
-          <p className="text-primary opacity-[60%] leading-5 py-6">
-            Browse through our diverse range of meticulously crafted garments,
-            designed <br /> to bring out your individuality and cater to your
-            sense of style.
-          </p>
-          <button className="btn font-light text-whiteFont bg-black rounded-full px-[54px] py-[16px]">
+        <div className="flex flex-col items-center md:items-start px-[20px] md:bp my-12">
+          <div>
+            <h1 className="font-heading text-[2.25rem] lg:text-[4rem] leading-none text-left">
+              FIND CLOTHES <br /> THAT MATCHES <br /> YOUR STYLE
+            </h1>
+            <p className="text-primary opacity-[60%] leading-5 py-6 text-[.875rem] md:text-[1rem] ">
+              Browse through our diverse range of meticulously crafted garments,
+              designed <br /> to bring out your individuality and cater to your
+              sense of style.
+            </p>
+          </div>
+          <button className="btn font-light text-whiteFont bg-black rounded-full px-[54px] py-[16px] w-[300px] md:w-[210px]">
             Shop Now
           </button>{" "}
           <div className="py-6 z-8">
-            <ul className="flex gap-8 heroList">
+            <ul className="flex flex-wrap justify-center gap-8 heroList">
               <li>
-                <h2>200+</h2>
+                <h2 className="text-[24px] font-bold leading-8 md:text-[40px] md:font-bold md:leading-10">200+</h2>
                 <p>International Brands</p>
               </li>
               <img src={Line} />
               <li>
-                <h2>2,000+</h2>
+                <h2 className="text-[24px] font-bold leading-8 md:text-[40px] md:font-bold md:leading-10">2,000+</h2>
                 <p>High-Quality Products</p>
               </li>
               <img src={Line} />
               <li>
-                <h2>30,000+</h2>
+                <h2 className="text-[24px] font-bold leading-8 md:text-[40px] md:font-bold md:leading-10">30,000+</h2>
                 <p>Happy Customers</p>
               </li>
             </ul>
